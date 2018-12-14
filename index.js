5570914a74f44c73870d0891a589e03d8fb85bcc
wolfx <digimonx@qq.com>
2018-12-14 15:05:00 +0800

    修复es6转义问题

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 require("babel-register")({
   presets: ["env", "flow"],
-  plugins: ["syntax-async-functions", "transform-regenerator", "babel-polyfill"]
+  plugins: ["syntax-async-functions", "transform-regenerator", "babel-polyfill"],
+  ignore: function(filename) {
+    if (filename.indexOf("@wolfx/lightning") > -1 || filename.indexOf("node_modules") === -1) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 });
 const core = require("./core/core");
 module.exports = {
