fa6b3093ca74a0456ef703d74d025cc2e37418e5
wolfx <digimonx@qq.com>
2019-03-30 12:41:39 +0800

    1.2.6: 完善 d.ts

---
+++ types/index.d.ts
@@ -0,0 +1,34 @@
+import NodeBatisLite, { NodeBatisLiteConfig } from "@wolfx/nodebatis-lite";
+import { Application } from "express";
+import { Instance as MulterInstance } from "multer";
+
+export interface LightningCorsConfig {
+  allowedOrigins: Array<string>
+}
+
+export interface LightningConfig {
+  database: NodeBatisLiteConfig;
+  cors: LightningCorsConfig;
+  storage: string;
+  yaml: string;
+  routerDir: string;
+}
+
+export interface LightningState {
+  app: Application;
+  upload: MulterInstance;
+  database: NodeBatisLite;
+  config: LightningConfig;
+}
+
+export interface LightningCore {
+  setConfig(cfg: LightningConfig): void;
+
+  start(port: number, callback: Function): void;
+
+  getState(): LightningState
+}
+
+export default class Lightning {
+  static core: LightningCore;
+}
