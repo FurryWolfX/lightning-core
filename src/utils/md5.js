a5e594f5fbb926af820ec320f3f68b99781cca83
wolfx <digimonx@qq.com>
2019-04-04 11:31:52 +0800

    use recommend buffer api

--- src/utils/md5.js
+++ src/utils/md5.js
@@ -2,9 +2,12 @@ const Buffer = require("buffer").Buffer;
 const crypto = require("crypto");
 
 function md5(data) {
-  const buf = new Buffer(data);
+  const buf = Buffer.from(data);
   const str = buf.toString("binary"); // 关键
-  return crypto.createHash("md5").update(str).digest("hex");
+  return crypto
+    .createHash("md5")
+    .update(str)
+    .digest("hex");
 }
 
 module.exports = md5;
